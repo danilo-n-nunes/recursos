265ddc5799118f5fecaf0db398504870dd741391
Danilo Nunes <danilowebdev@gmail.com>
2023-05-03 10:26:19 -0300

    refout

---
+++ RefEOut/RefEOut/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefEOut {
+    class Calculator {
+
+        public static void Triple(ref int x) {
+            x = x * 3;
+        }
+
+        public static void Triple(int x, out int y) {
+            y = x * 3;
+        }
+    }
+}
---
+++ RefEOut/RefEOut/Program.cs
@@ -0,0 +1,16 @@
+﻿using RefEOut;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            int a = 10;
+            int tripple;
+
+            Calculator.Triple(ref a);
+            Calculator.Triple(a, out tripple);
+            Console.WriteLine(a);
+            Console.WriteLine(tripple);
+        }
+    }
+}
